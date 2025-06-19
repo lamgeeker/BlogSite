b47fd2400c6895cf3d9bab15207e916cad648ae8
Maxym Kosyk <geekpr0gram7@gmail.com>
2025-06-19 11:36:44 +0200

    chore: clean up tracked temp files

---
+++ Blog/BlogSite.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostInfo;
+
+namespace Blog
+{
+    class BlogSite
+    {
+        
+
+        public void AddPost(string Title, string Content, Author author, List<Post> Posts, string created)
+        {
+            Random rnd = new Random();
+            //DateTime created = DateTime.Today;
+            int id = rnd.Next(1, 100000);
+            Posts.Add(new Post {  Title = Title, Content = Content , CreatedAt = DateTime.Parse(created), Id = id, Author = author});
+
+        }
+
+        public void PostList(List<Post> Posts)
+        {
+            foreach (Post post in Posts)
+            {
+                Console.WriteLine($"------------------------\nАвтор: {post.Author.Name } {post.Author.Surname} ID: {post.Id}\n{post.Title}\n{post.Content}\nЧас викладення статті:{post.CreatedAt}\n------------------------\n");
+            }
+        }
+        public List<Post> SortByTime(List<Post> posts)
+        {
+
+            return posts.OrderBy(p => p.CreatedAt).ToList();
+        }
+
+        public void ShowSortedList(List<Post> posts)
+        {
+            foreach (Post post in posts)
+            {
+                Console.WriteLine($"------------------------\nАвтор: {post.Author.Name} {post.Author.Surname} ID: {post.Id}\n{post.Title}\n{post.Content}\nЧас викладення статті:{post.CreatedAt}\n------------------------\n");
+            }
+        }
+        
+
+
+    }
+}
---
+++ Blog/Program.cs
@@ -0,0 +1,33 @@
+﻿using Blog;
+using PostInfo;
+
+List<Post> posts = new List<Post>();
+BlogSite blog = new BlogSite();
+while (true)
+{
+    Console.WriteLine("1----Додати новий пост    2-----Вивести всі пости   3-----Посортувати пости за датою створення і вивсети відсортований список");
+    int choice = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("Введіть заголовок статті");
+            string title = Console.ReadLine();
+            Console.WriteLine("Напишіть контент статті");
+            string content = Console.ReadLine();
+            Console.WriteLine("Напишіть ім'я та прізвище автора");
+            string Name = Console.ReadLine();
+            string Surname = Console.ReadLine();
+            Console.WriteLine("Напишіть дату створення поста (день.місяць.рік 12:30:39(на приклад)");
+            string createdAt = Console.ReadLine();
+            blog.AddPost(title, content, new Author() { Name = Name, Surname = Surname }, posts, createdAt);
+            break;
+        case 2:
+            blog.PostList(posts);
+            break;
+
+        case 3:
+            blog.ShowSortedList(blog.SortByTime(posts));
+            break;
+    }
+}
